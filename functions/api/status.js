854ebfd082d5cf20234d0a209cbacd1dc90c375b
Legal Support Help <team-litigation@legalsupporthelp.com>
2026-09-17 12:22:50 -0400

    Delete functions directory

--- functions/api/status.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  return new Response(JSON.stringify({ 
-    message: "Hello from Cloudflare serverless code!" 
-  }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
